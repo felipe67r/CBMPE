87c9d6f7c93bb18de59bcdc98c6514e1ca50c429
felipe67r <feliperibeiro67r@gmail.com>
2026-06-04 12:41:29 -0300

    Finalizando front-end da tela de Evidências

--- front-end/src/app/pages/evidencias/evidencias.page.ts
+++ front-end/src/app/pages/evidencias/evidencias.page.ts
@@ -1,22 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonToolbar, IonFooter, IonItem, IonLabel, IonInput, IonSelect, IonSelectOption, IonTextarea } from '@ionic/angular/standalone';
+import { IonicModule, NavController } from '@ionic/angular';
+import { RouterLink } from '@angular/router';
 import { MainHeaderComponent } from 'src/app/components/main-header/main-header.component';
-import { NavButtonComponent } from 'src/app/components/nav-button/nav-button.component';
+import { addIcons } from 'ionicons';
+import { arrowBack, arrowDown, cameraOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-evidencias',
   templateUrl: './evidencias.page.html',
   styleUrls: ['./evidencias.page.scss'],
   standalone: true,
-  imports: [IonContent, IonToolbar, CommonModule, FormsModule, MainHeaderComponent, NavButtonComponent, IonFooter, IonItem, IonLabel, IonInput, IonSelect, IonSelectOption, IonTextarea]
+  imports: [IonicModule, CommonModule, FormsModule, MainHeaderComponent, RouterLink]
 })
 export class EvidenciasPage implements OnInit {
+  fotosPreview: number[] = [1, 2, 3, 4, 5, 6];
+  detalhamentoTecnico: string = '';
+  agenteExtintor: string = '';
+  mangueiras: string = '';
+  protecaoRespiratoria: string = '';
+  arrombamento: string = '';
+  suporteBasicoVida: string = '';
+  transporte: string = '';
+  curativosEConsumiveis: string = '';
+  diagnostico: string = '';
+  ferramentasHidraulicas: string = '';
+  iluminacaoEnergia: string = '';
+  superficie: string = '';
+  mergulho: string = '';
+  cordas: string = '';
+  seguranca: string = '';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private navCtrl: NavController) {
+    addIcons({ arrowBack, arrowDown, cameraOutline });
   }
 
+  ngOnInit() {}
+
+  voltar() {
+    this.navCtrl.back();
+  }
+
+  abrirCamera() {
+    // Lógica do Capacitor Camera Plugin
+    console.log('Câmera acionada!');
+  }
 }
