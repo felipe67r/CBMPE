d30e56e4014abacc4508d7d3d61e512eaa94419c
devjonatas4 <jonatas2044@gmail.com>
2026-06-05 11:28:34 -0300

    deleção de front-end para implementação de mudanças

--- front-end/.angular/cache/20.3.25/app/vite/deps/p-BmVRXR1y-FKME22HX.js
+++
@@ -1,7 +0,0 @@
-import {
-  e
-} from "./chunk-QFJV73HH.js";
-import "./chunk-QHQP2P2Z.js";
-export {
-  e as startFocusVisible
-};
--- front-end/.angular/cache/20.3.25/app/vite/deps/p-Cl0B-RWe-6KNZOHZQ.js
+++
@@ -1,9 +0,0 @@
-import {
-  h,
-  s
-} from "./chunk-OST236JT.js";
-import "./chunk-QHQP2P2Z.js";
-export {
-  h as GESTURE_CONTROLLER,
-  s as createGesture
-};
